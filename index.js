975638ed5840abf7f77b63abff0ec948042209fb
JosueFS <josue.18rj@gmail.com>
2022-02-25 10:16:02 -0300

    config: initial configs to run server

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const { Neo4jGraphQL } = require("@neo4j/graphql");
+const { ApolloServer, gql } = require("apollo-server");
+const neo4j = require("neo4j-driver");
+
+const typeDefs = require("./src/types");
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(
+    process.env.NEO4J_USER,
+    process.env.NEO4J_PASSWORD
+  ));
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+neoSchema.getSchema().then((schema) => {
+  const server = new ApolloServer({
+    schema,
+  });
+
+  server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+})
