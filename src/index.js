73dc4911c4377680625329078e25ed0be67bc275
Ribeirorodrigo05 <Ribeiro.rodrigo05@gmail.com>
2022-03-25 15:58:14 -0300

    v1.0.2
    npm i axios

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const neo4j = require('neo4j-driver');
 
 const typeDefs = require('./types');
 
+
+
 const driver = neo4j.driver(
   process.env.NEO4J_URI,
   neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
